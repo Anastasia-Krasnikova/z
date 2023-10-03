1dd6e49741135158ecadf42c1228dcbc5a98a5db
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:04:24 +0300

    уравнение 2
    уравнение 2

---
+++ занятие 2/z2.py
@@ -0,0 +1,11 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import math
+x = 3.74*10**-2
+y = -0.825
+z = 0.16*10**2
+a = (1+math.sin(x+y)**2)
+b = abs(x-2*y/(1+x**2*y**2))
+c = math.cos(math.atan(1/z))**2
+s = a/b*(x**abs(y))+c
+print(s)
