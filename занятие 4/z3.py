df90ff6f565be0893e04749d14de909c13ea8c26
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:41:13 +0300

    Update z3.py

--- занятие 4/z3.py
+++ занятие 4/z3.py
@@ -1,3 +1,9 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-пр
+# -- coding: utf-8 —
+a=int(input())
+b=int(input())
+while a>b:
+    if a%2 !=0:
+        print(a)
+    a-=1
+    if b%2 !=0:
+        print(b)
