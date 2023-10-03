770262e1bd4d3d6a3406f5e282b75f45cc9c5383
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 15:30:29 +0300

    задание 6
    задание 6

---
+++ задание 1/z6.py
@@ -0,0 +1,8 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 9
+y = 8
+print(x, y)
+x, y = y, x
+print(x, y)
+print("   ")
