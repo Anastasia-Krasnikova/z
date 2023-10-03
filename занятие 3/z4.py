5e5b6a498eb04119bff79af489f63f9d8879b6a5
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:29:48 +0300

    Update z4.py

--- занятие 3/z4.py
+++ занятие 3/z4.py
@@ -1,3 +1,10 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-пппп
+# -- coding: utf-8 —
+a = int(input())
+b = int(input())
+l = int(input())
+N = int(input())
+def F(x,y,z,u):
+    return (((u/2)-1)*x*2) + (((u/2)-1)* y * 2) + y + (2 * z)
+def S(x,y,z):
+    return min(x,y,z)
+print(S(a,b,l))
