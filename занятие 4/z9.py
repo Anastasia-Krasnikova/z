3724e689f9cf6593f4ff52df3a7d36e3881614ad
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:58:38 +0300

    Update z9.py

--- занятие 4/z9.py
+++ занятие 4/z9.py
@@ -1,3 +1,7 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-пр
+# -- coding: utf-8 —
+a = b =1 
+N = int(input())-2
+print(a,b,end = ' ')
+for i in range(2,N):
+    a,b = b, a +b
+    print(b,end = ' ')
