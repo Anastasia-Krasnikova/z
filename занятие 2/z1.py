ae258fe754ab55550172f370b1a309f8112d98fe
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:02:13 +0300

    Update z1.py

--- занятие 2/z1.py
+++ занятие 2/z1.py
@@ -1,11 +1,11 @@
 # -- coding: utf-8 —
- import math
- x = 14.26
- y = - 1.22
- z = 3.5*10^-2
- a1 = 2*math.cos(x-2/3)
- b1 = 1/2 + math.sin(y)^2
- c1 = (1 +(z^2)/(3-(z^2)/5))
- a2 = a1/b1
- d = a2 * c1
- print(d)
+import math
+x = 14.26
+y = - 1.22
+z = 3.5*10^-2
+a1 = 2*math.cos(x-2/3)
+b1 = 1/2 + math.sin(y)^2
+c1 = (1 +(z^2)/(3-(z^2)/5))
+a2 = a1/b1
+d = a2 * c1
+print(d)
