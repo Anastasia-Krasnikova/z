bd6557e0e732f39be6db2ddce4ab73bcead130b7
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:57:37 +0300

    Update z8.py

--- занятие 4/z8.py
+++ занятие 4/z8.py
@@ -1,3 +1,7 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-пр
+# -- coding: utf-8 —
+n = int(input())
+if n <=9:
+    for i in range(n):
+        for j in range(1, i+1):
+            print(j , sep = '', end = '' )
+        print()
