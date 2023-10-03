a8e52f1a3f602420b2cdcb95bf569663ea5aa390
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:27:50 +0300

    Update z2.py

--- занятие 3/z2.py
+++ занятие 3/z2.py
@@ -1,3 +1,4 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-пппп
+# -- coding: utf-8 —
+a = int(input())
+b = int(input())
+print(1/2 *a*b)
