c4065e34b5c05f02e6a8bc7af06eedb3fa4bbe8f
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:45:23 +0300

    Update z5.py

--- занятие 4/z5.py
+++ занятие 4/z5.py
@@ -1,3 +1,6 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-пр
+# -- coding: utf-8 —
+n=int(input())
+s=0
+for i in range(1,n+1):
+    s += i^3
+    print(s)
