4047b138aa17d88503fe9b3c0ee334abfc4feac9
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 15:25:26 +0300

    Update z5.py

--- задание 1/z5.py
+++ задание 1/z5.py
@@ -1,3 +1,4 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# -- coding: utf-8 —
 n = 2
+print(n + n^2 + n^3 + n^4 + n^5)
+print("   ")
