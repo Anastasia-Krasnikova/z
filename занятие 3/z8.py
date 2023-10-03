b8b0cb9e20cc5eb74ef8c911a369014ada7806ef
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:35:23 +0300

    Update z8.py

--- занятие 3/z8.py
+++ занятие 3/z8.py
@@ -1,3 +1,10 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-пппп
+# -- coding: utf-8 —
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b ==c:
+    print(3)
+if a == b or b ==c or a == c:
+    print(2)
+else:
+    print(0)
