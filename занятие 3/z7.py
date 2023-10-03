dffbd48809f6fd7794c8cae552a8f86b6a3ded06
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 16:33:04 +0300

    Update z7.py

--- занятие 3/z7.py
+++ занятие 3/z7.py
@@ -1,3 +1,9 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-пппп
+# -- coding: utf-8 —
+a = int(input())
+def F(b):
+    if b%4 == 0 and b%100!=0:
+        print('да')
+    elif b%400 ==0:
+        print('нет')
+    else:
+        print('нет')
