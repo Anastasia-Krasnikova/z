6282f82826b9b577434eb89d5e6461bf192b3901
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-10-03 15:40:34 +0300

    задание 7
    задание 7

---
+++ задание 1/z7.py
@@ -0,0 +1,8 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+number = 17
+if number%2 ==0:
+    print(True)
+    else:
+        
+SyntaxError: invalid syntax
