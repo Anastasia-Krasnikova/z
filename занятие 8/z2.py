8a7ab82d37be4940303ba0e6e21b6a21cd6f7402
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-11-04 22:34:25 +0300

    Update z2.py

--- занятие 8/z2.py
+++ занятие 8/z2.py
@@ -1,27 +1,25 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 def p(massif):
     pr = 1
     for num in massif:
         pr *= num
-        return pr
-    def a(massif):
-        sum = 0
-        for num in massif:
-            sum += num
-            return sum / len(massif)
-        massif1 = [2,4,6,8]
-        massif2 = [1,3,5,7]
-        massif3 = [10,20,30,40]
-        pr1 = p(massif1)
-        pr2 = p(massif2)
-        pr3 = p(massif3)
-        a1 = a(massif1)
-        a2 = a(massif2)
-        a3 = a(massif3)
-        print("Pr of massif1:", pr1)
-        print("Pr of massif2:", pr2)
-        print("Pr of massif3:", pr3)
-        print("A of massif1:", a1)
-        print("A of massif2:", a2)
-        print("A of massif3:", a3)
+    return pr
+def a(massif):
+    sum = 0
+    for num in massif:
+        sum += num
+    return sum / len(massif)
+    massif1 = [2,4,6,8]
+    massif2 = [1,3,5,7]
+    massif3 = [10,20,30,40]
+    pr1 = p(massif1)
+    pr2 = p(massif2)
+    pr3 = p(massif3)
+    a1 = a(massif1)
+    a2 = a(massif2)
+    a3 = a(massif3)
+    print("Pr of massif1:", pr1)
+    print("Pr of massif2:", pr2)
+    print("Pr of massif3:", pr3)
+    print("A of massif1:", a1)
+    print("A of massif2:", a2)
+    print("A of massif3:", a3)
