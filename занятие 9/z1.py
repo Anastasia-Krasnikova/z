bdc33a072b45e9ebe275ca8dfdef6d24ef589239
Anastasia-Krasnikova <146817805+Anastasia-Krasnikova@users.noreply.github.com>
2023-11-18 20:00:14 +0300

    Update z1.py

--- занятие 9/z1.py
+++ занятие 9/z1.py
@@ -1,5 +1,3 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 def code(matrix, k):
     c = 0
     max_element = None
@@ -9,14 +7,11 @@ def code(matrix, k):
                 c += 1
                 if max_element is None or element > max_ekement:
                     max_element = element
-
-                    
-return c, max_element
-SyntaxError: 'return' outside function
+    return c, max_element
 def m():
     martix = [[2,4,6],[9,12,15],[10,20,30]]
     k = 3
     c, max_element = code(matrix, k)
-    print("Число элементов, кратных",k,":", c)
+    print("Число элементов, кратных", k, ":", c)
     print("Наибольший из этих элиментов:", max_element)
     m()
